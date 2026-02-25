1f39bdd79b567c6f98114f53815222e0d5c46df4
nabudahab <nabudaha@purdue.edu>
2026-02-25 09:06:35 -0500

    Embassy-nrf setup

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,29 @@
 #![no_std]
 #![no_main]
+#![macro_use]
 
+use core::mem;
+
+use defmt::{info, *};
 use embassy_executor::Spawner;
-use embassy_nrf::gpio::{Level, Output, OutputDrive};
-use embassy_time::Timer;
+use nrf_softdevice::ble::{central, gatt_client, Address, AddressType};
+use nrf_softdevice::{raw, Softdevice};
+
 use {panic_halt as _};
+
+#[embassy_executor::task]
+async fn softdevice_task(sd: &'static Softdevice) -> ! {
+    sd.run().await
+}
+
+#[nrf_softdevice::gatt_client(uuid = "180d")]
+struct HeartRateClient {
+    #[characteristic(uuid = "2a37", notify)]
+    raw_HR: [u8; 5]
+
+    #[characteristic(uuid = "2a39", write)]
+    resetEnergyExpended: u8,
+}
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
